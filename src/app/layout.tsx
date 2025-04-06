9c7b28ce46436cc7acb72a48585367e63127b2a0
Kanish Mahato <kanishm245@gmail.com.com>
2025-04-06 19:42:45 +0530

    search Bar added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Icons } from "@/components/Icons";
 
+import { SearchBar } from "@/components/SearchBar";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -50,11 +52,17 @@ export default function RootLayout({
         </svg>
           <div className="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-32 lg:flex gap-16 lg:px-8 lg:py-24" >
             <div className="h-full w-full flex flex-col items-center gap-4">
-
+              <Icons.Sparkles className="h-16 w-16" />
               <h1 className="tracking-tight text-4xl sm:text-6xl font-bold ">
 
                 MagicSearch
               </h1>
+              <p className="text-slate-700 text-lg text-center max-w-xl">Hybrid Search Engine</p>
+            
+              <div className="mx-auto mt-16 w-full max-w-2xl flex flex-col">
+                <SearchBar/>
+                { children}
+            </div>
             </div>
 
           </div>
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,21 @@
+import { Input } from "./ui/input"
+
+export const SearchBar = () => {
+
+    return (
+
+        <div className="relative w-full h-15 flex flex-col bg-white">
+
+            <div className="relative
+h-14 z-10 rounded-lg m-1 border-amber-950">
+                <Input className="h-15 w-max-full flex-auto items-center border-amber-300 border-2" />
+
+
+
+
+            </div>
+
+        </div>
+    )
+
+}
