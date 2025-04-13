007b72c1e57ba94480aaff1a1d55bd91cca27d4c
Kanish Mahato <kanishm245@gmail.com.com>
2025-04-13 20:20:23 +0530

    till api key done

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,23 +1,31 @@
-import { redirect } from "next/navigation"
+import { db } from "@/db";
+import { productsTable } from "@/db/schema";
+import { redirect } from "next/navigation";
+import { sql } from "drizzle-orm";
 
-interface PageProps { 
-
+interface PageProps {
     searchParams: {
-
-        [key:string]:string |string[]|undefined
-    }
+        [key: string]: string | string[] | undefined;
+    };
 }
 
-const Page = ({searchParams}:PageProps) => { 
-    const query = searchParams.query
-    if (Array.isArray(query) || !query) { 
+const Page = async ({ searchParams }: PageProps) => {
+    const query = searchParams.query;
 
-        return redirect('/')
+    if (Array.isArray(query) || !query) {
+        return redirect('/');
     }
-    //querying logic
-    
 
-return <p>hello from after searching call</p>
+    const cleanedQuery = query.trim().split(/\s+/).join(' & ');
 
-}
-export default Page
+    const products = await db
+        .select()
+        .from(productsTable)
+        .where(
+            sql`to_tsvector('simple', lower(${productsTable.name} || ' ' || ${productsTable.description})) @@ to_tsquery('simple', ${cleanedQuery})`
+        ).limit(3);
+
+    return <pre>{ JSON.stringify(products)}</pre>;
+};
+
+export default Page;
---
+++ src/lib/openai.ts
@@ -0,0 +1,7 @@
+import "dotenv/config";
+
+import OpenAi from "openai"
+export const openai = new OpenAi({
+
+    apiKey:process.env.OPENAI_API_KEY!,
+})
